f3281b73078395baa956deae3426ece9afc6a211
Periklis Pan <p.panagiotidis@beatthewhites.com>
2024-11-21 15:21:00 +0200

    theme // working on assets

--- wp-content/themes/contra/header.php
+++ wp-content/themes/contra/header.php
@@ -15,7 +15,6 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200;700;800&display=swap" rel="stylesheet">
 
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
