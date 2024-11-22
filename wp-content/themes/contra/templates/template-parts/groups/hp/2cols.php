b11ddbba1efdc53abb6cd4004c09065a93c08111
c-pechlevanoudis-btw <c.pechlevanoudis@beatthewhites.com>
2024-11-22 14:55:59 +0200

    HP // New group templates

---
+++ wp-content/themes/contra/templates/template-parts/groups/hp/2cols.php
@@ -0,0 +1,34 @@
+<?php
+$group_settings = btw_get_group_settings();
+extract($group_settings);
+
+
+$atf_posts = $posts_source ? null : get_field( 'btw__group_fields__hp__template__2cols__posts_selection' );
+$btw_posts = btw_get_group_posts(2, $atf_posts, $posts_source);
+
+
+
+$section_classes = [
+	$group_template,
+];
+
+?>
+
+<section id="<?php echo $section_id; ?>" class="<?php echo implode(' ', $section_classes); ?>">
+
+	<?php btw_get_impressions_url($impressions_url); ?>
+
+	<?php foreach($btw_posts as $btw_post): ?>
+        <?php
+        $btw_post->set_args([
+            'render_attrs'      => [
+
+            ],
+            //'image_srcsets' => array()
+        ]);
+        $btw_post->render();
+        ?>
+    <?php endforeach; ?>
+
+</section>
+
