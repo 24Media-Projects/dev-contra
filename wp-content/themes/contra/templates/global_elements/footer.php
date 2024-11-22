69b21993b995a4d960727bb1e11b4816fe17737f
Periklis Pan <p.panagiotidis@beatthewhites.com>
2024-11-22 13:55:46 +0200

    footer // working on footer

--- wp-content/themes/contra/templates/global_elements/footer.php
+++ wp-content/themes/contra/templates/global_elements/footer.php
@@ -1,143 +1,93 @@
 <?php
-$logo = get_field('btw__brand_fields__logo', 'option');
+$facebook = get_field('btw__brand_fields__facebook', 'option');
+$instagram = get_field('btw__brand_fields__instagram', 'option');
+$twitter = get_field('btw__brand_fields__twitter', 'option');
 $owner_logo = get_field('btw__brand_fields__owner_logo', 'option');
 $owner_logo_img = $owner_logo['image'];
 $owner_logo_link = $owner_logo['link'];
 ?>
 
-<footer class="global_footer">
+<footer id="global__footer" class="global__footer">
+
     <div class="wrapper">
-        <div class="footer-menu__container">
-            <div class="container">
-                <div class="footer-menu__logo">
-                    <figure class="global_footer__logo">
-                        <a href="/">
-                            <img src="<?php echo $logo['url']; ?>" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" width="131" />
-                        </a>
-                    </figure>
-                </div>
-                <div class="footer-menu__groups">
-                    <div id="footer-menu__group_1" class="footer-menu__group menu-item-has-children">
-                        <span class="footer_menu__button footer_menu__button--toggle_sub_menu" on="tap:footer-menu__group_1.toggleClass(class='open-sub-menu')">
-                            ΑΘΛΗΜΑΤΑ
-                            <svg class="icon">
-                                <use xlink:href="#icon-plus"></use>
-                                <use xlink:href="#icon-minus"></use>
-                            </svg>
-                        </span>
-                        <nav class="footer-menu__group-items">
-							<?php wp_nav_menu([
-								'theme_location' => 'footer_menu__sports',
-								'container' => false,
-							]); ?>
-                        </nav>
-                    </div>
-                    <div id="footer-menu__group_2" class="footer-menu__group menu-item-has-children">
-                        <span class="footer_menu__button footer_menu__button--toggle_sub_menu" on="tap:footer-menu__group_2.toggleClass(class='open-sub-menu')">
-                            ΔΙΟΡΓΑΝΩΣΕΙΣ
-                            <svg class="icon">
-                                <use xlink:href="#icon-plus"></use>
-                                <use xlink:href="#icon-minus"></use>
-                            </svg>
-                        </span>
-                        <nav class="footer-menu__group-items">
-							<?php wp_nav_menu([
-								'theme_location' => 'footer_menu__competitions',
-								'container' => false,
-							]); ?>
-                        </nav>
-                    </div>
-                    <div id="footer-menu__group_3" class="footer-menu__group menu-item-has-children">
-                        <span class="footer_menu__button footer_menu__button--toggle_sub_menu" on="tap:footer-menu__group_3.toggleClass(class='open-sub-menu')">
-                            QUICKLINKS
-                            <svg class="icon">
-                                <use xlink:href="#icon-plus"></use>
-                                <use xlink:href="#icon-minus"></use>
-                            </svg>
-                        </span>
-                        <nav class="footer-menu__group-items">
-							<?php wp_nav_menu([
-								'theme_location' => 'footer_menu__quicklinks',
-								'container' => false,
-							]); ?>
-                        </nav>
-                    </div>
-                    <div id="footer-menu__group_4" class="footer-menu__group menu-item-has-children">
-                        <span class="footer_menu__button footer_menu__button--toggle_sub_menu" on="tap:footer-menu__group_4.toggleClass(class='open-sub-menu')">
-                            FOLLOW SPORT 24
-                            <svg class="icon">
-                                <use xlink:href="#icon-plus"></use>
-                                <use xlink:href="#icon-minus"></use>
-                            </svg>
-                        </span>
-                        <nav class="footer-menu__group-items footer-menu__group-items__social">
-							<?php get_template_part('templates/global_elements/main_menu/social_media'); ?>
-                        </nav>
-                    </div>
-                    <div id="footer-menu__group_5" class="footer-menu__group menu-item-has-children">
-                        <span class="footer_menu__button footer_menu__button--toggle_sub_menu" on="tap:footer-menu__group_5.toggleClass(class='open-sub-menu')">
-                            MOBILE APPLICATIONS
-                            <svg class="icon">
-                                <use xlink:href="#icon-plus"></use>
-                                <use xlink:href="#icon-minus"></use>
-                            </svg>
-                        </span>
-                        <nav class="footer-menu__group-items footer-menu__group-items__mobile_apps">
-							<?php wp_nav_menu([
-								'theme_location' => 'footer_menu__mobile_apps',
-								'container' => false,
-							]); ?>
-                        </nav>
-                    </div>
-                </div>
-                <div class="footer-menu__id-links">
-                    <nav class="footer_nav">
-						<?php wp_nav_menu([
-							'theme_location' => 'footer_menu__rest',
-							'container' => false,
-						]); ?>
-                    </nav>
-                </div>
-            </div>
+
+        <div class="global__footer--top">
+
+            <nav class="footer__primary_nav">
+                <?php wp_nav_menu([
+                    'theme_location' => 'footer_nav',
+                    'container' => false,
+                ]); ?>
+            </nav>
+
+            <nav class="footer__social_nav">
+                <ul class="menu">
+                    <?php if ($facebook) { ?>
+                        <li class="menu-item follow_us__link">
+                            <a class="facebook" title="Ακολουθήστε μας στο Facebook" href="<?php echo $facebook; ?>" target="_blank">
+                                <svg>
+                                    <use xlink:href="#icon-facebook"></use>
+                                </svg>
+                            </a>
+                        </li>
+                    <?php } ?>
+                    <?php if ($twitter) { ?>
+                        <li class="menu-item follow_us__link">
+                            <a class="twitter" title="Ακολουθήστε μας στο Twitter" href="<?php echo $twitter; ?>" target="_blank">
+                                <svg>
+                                    <use xlink:href="#icon-twitter"></use>
+                                </svg>
+                            </a>
+                        </li>
+                    <?php } ?>
+                    <?php if ($instagram) { ?>
+                        <li class="menu-item follow_us__link">
+                            <a class="instagram" title="Ακολουθήστε μας στο Instagram" href="<?php echo $instagram; ?>" target="_blank">
+                                <svg>
+                                    <use xlink:href="#icon-instagram"></use>
+                                </svg>
+                            </a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </nav>
+
         </div>
-        <div class="footer-menu__bottom">
-            <div class="container">
 
+        <div class="global__footer--bottom">
 
-                <div class="media24_ened_logos">
-                    <div>
-                        <p>SITE ΤΟΥ ΟΜΙΛΟΥ 24MEDIA</p>
-						<?php if ($owner_logo_img) { ?>
-                            <figure class="owner_logo">
-								<?php if ($owner_logo_link) { ?>
-                                    <a href="<?php echo $owner_logo_link; ?>" target="_blank" aria-label="Owner Logo">
-                                        <img src="<?php echo $owner_logo_img['url']; ?>" alt="<?php echo $owner_logo_img['alt']; ?>" width="53">
-                                    </a>
-								<?php } else { ?>
+            <div class="media24_ened_logos">
+
+                <div>
+                    <p>SITE ΤΟΥ ΟΜΙΛΟΥ 24MEDIA</p>
+                    <?php if ($owner_logo_img) { ?>
+                        <figure class="owner_logo">
+                            <?php if ($owner_logo_link) { ?>
+                                <a href="<?php echo $owner_logo_link; ?>" target="_blank" aria-label="Owner Logo">
                                     <img src="<?php echo $owner_logo_img['url']; ?>" alt="<?php echo $owner_logo_img['alt']; ?>" width="53">
-								<?php } ?>
-                            </figure>
-						<?php } ?>
-                    </div>
+                                </a>
+                            <?php } else { ?>
+                                <img src="<?php echo $owner_logo_img['url']; ?>" alt="<?php echo $owner_logo_img['alt']; ?>" width="53">
+                            <?php } ?>
+                        </figure>
+                    <?php } ?>
+                </div>
 
-                    <div>
-                        <p>ΜΕΛΟΣ</p>
-                        <a href="http://www.ened.gr/" target="_blank" aria-label="Ened">
-                            <svg class="ened_logo">
-                                <use xlink:href="#ened-logo"></use>
-                            </svg>
-                        </a>
-                    </div>
+                <div>
+                    <p>ΜΕΛΟΣ</p>
+                    <a href="http://www.ened.gr/" target="_blank" aria-label="Ened">
+                        <svg class="ened_logo">
+                            <use xlink:href="#ened-logo"></use>
+                        </svg>
+                    </a>
                 </div>
-                <div class="footer-menu__network-links">
-                    <nav class="footer_nav">
-						<?php wp_nav_menu([
-							'theme_location' => 'footer_menu__properties',
-							'container' => false,
-						]); ?>
-                    </nav>
-                </div>
-                <span class="footer-menu__copyright">COPYRIGHT &copy; <?php echo date("Y"); ?> SPORT24 ALL RIGHTS RESERVED</span>
             </div>
+
+            <p class="copyrights">
+                <span>ΑΡΙΘΜΟΣ ΠΙΣΤΟΠΟΙΗΣΗΣ Μ.Η.Τ 232312</span>
+                <span>&copy; <?php echo date("Y"); ?> CONTRA. ALL RIGHTS RESERVED.</span>
+            </p>
+
         </div>
+
 </footer>
