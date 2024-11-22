eb4d30425f7cc94d500686bbc7205df9008a9134
Periklis Pan <p.panagiotidis@beatthewhites.com>
2024-11-22 17:30:13 +0200

    modules // add base styles

--- wp-content/themes/contra/templates/template-parts/modules/article_default.php
+++ wp-content/themes/contra/templates/template-parts/modules/article_default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Set truncate lines as data attribute, if requested
  */
@@ -8,17 +9,18 @@ $truncate_data_lines = $truncate ? 'data-truncate-lines="' . $truncate . '"' : '
 
 <article class="<?php echo implode(' ', $container_classes); ?>">
 
-	<?php btw_get_impressions_url($impressions_url); ?>
+    <?php btw_get_impressions_url($impressions_url); ?>
 
-    <?php if($caption): ?>
+    <?php if ($caption): ?>
         <div class="article_card__caption">
-            <?php echo $caption; // <a> or plain text ?>
+            <?php echo $caption; // <a> or plain text 
+            ?>
         </div>
     <?php endif; ?>
 
     <figure class="article_card__image">
         <a <?php maybe_print_target_blank($post_link); ?> href="<?php echo $post_link; ?>" title="<?php echo $esc_post_title; ?>">
-			<?php echo $attachment_picture_html; ?>
+            <?php echo $attachment_picture_html; ?>
         </a>
     </figure>
 
@@ -27,22 +29,25 @@ $truncate_data_lines = $truncate ? 'data-truncate-lines="' . $truncate . '"' : '
         <h2 class="article_card__title">
 
             <a title="<?php echo $esc_post_title; ?>" <?php maybe_print_target_blank($post_link); ?> href="<?php echo $post_link; ?>">
-                <span class="desktop_title truncate" <?php echo $truncate_data_lines;?>><?php echo $post_titles['desktop']; ?></span>
+                <span class="desktop_title truncate" <?php echo $truncate_data_lines; ?>><?php echo $post_titles['desktop']; ?></span>
 
-				<?php if ($post_titles['mobile']) : ?>
-                    <span class="mobile_title truncate" <?php echo $truncate_data_lines;?>><?php echo $post_titles['mobile']; ?></span>
-				<?php endif; ?>
+                <?php if ($post_titles['mobile']) : ?>
+                    <span class="mobile_title truncate" <?php echo $truncate_data_lines; ?>><?php echo $post_titles['mobile']; ?></span>
+                <?php endif; ?>
             </a>
 
         </h2>
 
         <h3 class="article_card__author">
+            <svg>
+                <use xlink:href="#icon-author"></use>
+            </svg>
             <?php echo $author_html; ?>
         </h3>
 
-		<?php if ($post_date): ?>
+        <?php if ($post_date): ?>
             <span class="article_card__date"><?php echo btw_format_datetime($post_date); ?></span>
-		<?php endif; ?>
+        <?php endif; ?>
 
     </div>
 
